fc950a0a65fe344e5adaa11a6f51d7f7b8717b69
Sergio2490 <merido2490@gmail.com>
2024-12-23 00:36:08 +0300

    finish code  sOlid

--- part_2.py
+++ part_2.py
@@ -18,12 +18,30 @@ class Formatted(ABC):   #наследуем класс от абстракции
 
 class TextFormatted(Formatted):  #сначала создадим класс, к-й б. раб. с обычным текстом
     def format(self, report):
-        print()
-        print()
+        print(report.title)  #черезэтот объект клсса выводим Заголовок
+        print(report.content)
 
 class HtmlFormatted(Formatted):  #класс б. преобразовывать текст в html
     def format(self, report):
-        print()
-        print()
+        print(f"<h1>{report.title}</h1>")  #h1 - тэг заголовка, он двойной
+        print(f"<p>{report.content}</p>")  #p - тэг параграфа, это обычный текст
 
 class Report():  #класс, где б. вноситься вся инфоромация
+    def __init__(self, title, content, formatted):
+        self.title = title  #привязка хар-к к классу
+        self.content = content
+        self.formatted = formatted   #указываем здесь класс, к-1 нужно использовать, те будем создавать
+                               #объект класса , к-й б. передаваться в аргумент formatted класса Report
+    def docPrinter(self):
+        self.formatted.format(self)  #прописываем вызов ф-ции format() у нашей хар-ки formatted
+
+#Все классы готовы
+report = Report("заголовок отчета", "это текст отчета, его тут много", TextFormatted())
+        #указываем тут (3-й арг-нт) класс, к-й нужно использовать, те создаем объект класса, к-1 б.
+        # сохранятьсяв formatted (3-q fhu a-wbb штше rkfccf Кузщке
+# А сейчас используем объект класса report в ф-ции format (print) классов TextFormatted и HtmlFormatted
+
+report.docPrinter()  #мы вызываем ф-ю docPrinterЮ но она берет ф-цию format() того класса,
+           #к-й у нас прописан в formatted. Сейчас там прописан TextFormatted
+
+
