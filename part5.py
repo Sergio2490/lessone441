27ad4283fc8b84a80791acca3c79bfeadc7c25a8
Sergio2490 <merido2490@gmail.com>
2025-01-08 00:04:38 +0300

    example not correct principle D

---
+++ part5.py
@@ -0,0 +1,14 @@
+#D - принцип инверсии зависимости
+# сначала созд класс, к-й не использ д-й принцип
+# class Book():
+#     def read(self):
+#         print("Чтение интересной истории")
+#
+# class StoryReader():
+#     def __init__(self):
+#         self.book = Book()  # мы создали объект класса Book.
+#
+#     def tell_story(self):
+#         self.book.read()  #мы используем ф-ю read() из класса Book. Те, сейчас класс StoryReader
+#                         # сильно зависит от класса Book/ А так быть не должно!
+
