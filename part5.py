ea8901e30861141da47804623fa51d4946237bb6
Sergio2490 <merido2490@gmail.com>
2025-01-09 00:30:50 +0300

    Update part5.py
    начало правильного использования принципа D

--- part5.py
+++ part5.py
@@ -12,3 +12,9 @@
 #         self.book.read()  #мы используем ф-ю read() из класса Book. Те, сейчас класс StoryReader
 #                         # сильно зависит от класса Book/ А так быть не должно!
 
+from abc import ABC, abstractmethod  # подключаем возм-ть работы с абстрактными методами
+class StorySource(ABC): # абстракт класс, наследуется от ABC
+    @abstractmethod
+    def get_store(self):  # ф-я б. пустая, мы ее заполним в следующих наших классах
+        pass
+
