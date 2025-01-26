96bf3b60b850f4b2343bda0ff2dacef15a168f98
Sergio2490 <merido2490@gmail.com>
2025-01-27 00:12:01 +0300

    All_dz_OB04

--- вDzOB04_principle_O.py
+++ вDzOB04_principle_O.py
@@ -9,7 +9,7 @@ class Sword(Weapon):  # создали класс Меч
         print("Воин рубит мечом")
 class Bow(Weapon):  # создали класс Лук
     def attack(self):
-        print("Воин стрелт из лука")
+        print("Воин стреляет из лука")
 
 class Fighter:  # создали класс бойца. Если класс ни от чего не насл-ся, скобки м. не писать
     def __init__(self, weapon: Weapon):  # указываем, какой тип д.храниться в переменной
@@ -19,3 +19,20 @@ class Fighter:  # создали класс бойца. Если класс н
     def changeWeapon(self, weapon: Weapon): # еще дб ф-я смены оружия. Будем выбирать тип оружия класса Weapon
         self.weapon = weapon
 
+    def fight(self):  # осталось реализовать возможность воина использовать это оружие
+        print(self.weapon.attack()) #у нас уже есть реализация attack в дочерних классах Меч и Лук
+                           # в self.weapon хранится конкретное переданное оружие - Меч или Лук
+
+class Monster():
+    pass
+
+#Создадим объекты
+#Создадим объекты классов
+sword1 = Sword()  #об кл Меч
+bow1 = Bow()
+fighter = Fighter(sword1)  #созд  об кл Бойца и передали ему оружие Меч
+fighter.fight()
+
+# Сейчас сменим оружие:
+fighter.changeWeapon(bow1)  #заменили на лук
+fighter.fight()
